2d3d9b86ae6fa3b50a6f212eaffe757ceb642516
tjrwls0826 <tjrwls0826@naver.com>
2024-05-08 15:41:03 +0900

    SecurityConfig 생성

---
+++ src/main/java/com/example/oauthjwt/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.oauthjwt.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //csrf disable JWT를 발급해서 STATELESS 관리할거라 꺼도 됨
+        http
+                .csrf((auth) -> auth.disable());
+
+        //From 로그인 방식 disable JWT,OAuth를 통해서 로그인할꺼라 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //HTTP Basic 인증 방식 disable JWT,OAuth를 통해서 로그인할꺼라 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        //oauth2
+        http
+                .oauth2Login(Customizer.withDefaults());
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll() // 루트는 permitAll
+                        .anyRequest().authenticated()); // 나머지는 로그인한 사용자만 접근가능
+
+        //세션 설정 : STATELESS
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
