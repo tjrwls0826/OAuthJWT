a570b35a0cc88acf76b2d78f8df119a0af90c5a3
tjrwls0826 <tjrwls0826@naver.com>
2024-05-08 15:59:45 +0900

    구글 로그인 구현

---
+++ src/main/java/com/example/oauthjwt/dto/GoogleResponse.java
@@ -0,0 +1,35 @@
+package com.example.oauthjwt.dto;
+
+public class GoogleResponse implements OAuth2Response{
+
+    private final Map<String, Object> attribute;
+
+    public GoogleResponse(Map<String, Object> attribute) {
+
+        this.attribute = attribute;
+    }
+
+    @Override
+    public String getProvider() {
+
+        return "google";
+    }
+
+    @Override
+    public String getProviderId() {
+
+        return attribute.get("sub").toString(); // String으로 변환
+    }
+
+    @Override
+    public String getEmail() {
+
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+
+        return attribute.get("name").toString();
+    }
+}
