6dc349e63ec60bef03417f569e5a32550de003dd
tjrwls0826 <tjrwls0826@naver.com>
2024-05-08 17:11:39 +0900

    aws mysql 연결

--- src/main/java/com/example/oauthjwt/service/CustomOAuth2UserService.java
+++ src/main/java/com/example/oauthjwt/service/CustomOAuth2UserService.java
@@ -5,6 +5,8 @@ import com.example.oauthjwt.dto.CustomOAuth2User;
 import com.example.oauthjwt.dto.GoogleResponse;
 import com.example.oauthjwt.dto.OAuth2Response;
 import com.example.oauthjwt.dto.UserDTO;
+import com.example.oauthjwt.entity.UserEntity;
+import com.example.oauthjwt.repository.UserRespository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -13,6 +15,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRespository userRespository;
+
+    public CustomOAuth2UserService(UserRespository userRespository) {
+
+        this.userRespository = userRespository;
+    }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -40,12 +49,39 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         //리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디값을 만듬
         String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+        UserEntity exitData = userRespository.findByUsername(username);
 
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUsername(username);
-        userDTO.setName(oAuth2Response.getName());
-        userDTO.setRole("ROLE_USER");
+        //데이터가 존재하지 않는경우
+        if(exitData == null) {
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setRole("ROLE_USER");
 
-        return new CustomOAuth2User(userDTO);
+            userRespository.save(userEntity);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(username);
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDTO);
+        }
+        //이미 존재
+        else{
+
+            existData.setEmail(oAuth2Response.getEmail());
+            existData.setName(oAuth2Response.getName());
+
+            userRepository.save(existData);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(existData.getUsername());
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(userDTO);
+        }
     }
 }
